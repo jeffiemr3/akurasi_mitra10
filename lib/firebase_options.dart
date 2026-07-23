3249b5852311ccc613929138121ec54b473416ec
70004 <jeffie.mitrahman@gmail.com>
2026-07-23 18:21:05 +0700

    Add files via upload

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,18 +1,10 @@
-// File ini dibuat manual sebagai TITIK AWAL saja.
+// Konfigurasi Firebase project "akurasi-mitra10".
 //
-// SANGAT DISARANKAN untuk menggantinya dengan hasil generate resmi:
-//   dart pub global activate flutterfire_cli
-//   flutterfire configure
-//
-// Perintah di atas otomatis mengisi appId & measurementId yang benar untuk
-// tiap platform (web/android/ios) dan menaruh google-services.json /
-// GoogleService-Info.plist di tempat yang tepat.
-//
-// Nilai di bawah ini memakai projectId/apiKey/authDomain yang sudah ada dari
-// project sebelumnya (Firestore). Untuk Realtime Database, kamu WAJIB
-// mengisi `databaseURL` (lihat Firebase Console > Realtime Database > url
-// di bagian atas halaman, formatnya https://<project-id>-default-rtdb.
-// <region>.firebasedatabase.app).
+// File ini dibuat manual dari config yang diberikan (Firebase Console >
+// Project Settings > SDK setup and configuration). Kalau nanti kamu juga
+// mau build untuk Android/iOS, jalankan `flutterfire configure` supaya
+// appId khusus Android/iOS ikut terisi otomatis (saat ini android/ios masih
+// memakai config web sebagai fallback).
 
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
@@ -36,17 +28,18 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDb0HS3M_-e1tjxGmVXaYpweN_8ExrAMV0',
-    appId: '1:859391207472:web:db81eec614766288b5e5f5',
-    messagingSenderId: '859391207472',
-    projectId: 'arctic-totem-zj1d7',
-    authDomain: 'arctic-totem-zj1d7.firebaseapp.com',
-    storageBucket: 'arctic-totem-zj1d7.firebasestorage.app',
-    // TODO: isi dengan URL Realtime Database asli dari Firebase Console.
-    databaseURL: 'https://arctic-totem-zj1d7-default-rtdb.asia-southeast1.firebasedatabase.app',
+    apiKey: 'AIzaSyBXdX71IPsv7AvMYTJUUnIvyb3PRPRUmx8',
+    appId: '1:451811488658:web:68f1776af4fff3cc75074b',
+    messagingSenderId: '451811488658',
+    projectId: 'akurasi-mitra10',
+    authDomain: 'akurasi-mitra10.firebaseapp.com',
+    storageBucket: 'akurasi-mitra10.firebasestorage.app',
+    databaseURL: 'https://akurasi-mitra10-default-rtdb.firebaseio.com',
+    measurementId: 'G-PT2QBZZJ4G',
   );
 
-  // TODO: lengkapi android/ios lewat `flutterfire configure` jika dipakai.
+  // TODO: lengkapi android/ios lewat `flutterfire configure` kalau nanti
+  // mau build ke Android/iOS juga (appId-nya beda dari web).
   static const FirebaseOptions android = web;
   static const FirebaseOptions ios = web;
 }
